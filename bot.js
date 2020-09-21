9e6c290b0868c8d29f784da63a0dbdcfcf5af1b0
BuildTools <unconfigured@null.spigotmc.org>
2020-09-21 10:37:40 -0500

    commit on Commando

--- bot.js
+++ bot.js
@@ -12,7 +12,6 @@ const config = require('./config.json');
 const tag = require('./commands/tag');
 const disable = require('./commands/disable');
 const prefix = config.prefix;
-var yui = 'true';
 
 const client = new CommandoClient({ 
 	ws: { intents: Intents.ALL },
