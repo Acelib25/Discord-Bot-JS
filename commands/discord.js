38a466372218ad1785da920b3909fc69b7baf7dc
BuildTools <unconfigured@null.spigotmc.org>
2020-09-22 15:32:34 -0500

    added discord command

---
+++ commands/discord.js
@@ -0,0 +1,13 @@
+const { prefix } = require('../config.json');
+
+module.exports = {
+	name: 'discord',
+	description: 'He he advertising',
+	aliases: ['aceserver'],
+	usage: 'Yes',
+	cooldown: 5,
+	guildOnly: false,
+	execute(message, args) {
+		message.channel.send("Ace likes to have frens. Here is his server. https://discord.gg/q8qVCAq")
+	},
+};
