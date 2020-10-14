7f9d3c29efdde8956da7f188935cca4cc74173e7
BuildTools <unconfigured@null.spigotmc.org>
2020-10-14 10:43:18 -0500

    fixed bot mentions

--- bot.js
+++ bot.js
@@ -212,7 +212,8 @@ client.on('message', async message => {
 
 
 	if (message.channel.type != 'dm' && !disabledCommands.includes('botspeech')) {ambiance.execute(nsfwMode, message, args, logger)}
-	if (message.mentions.has(bot.user)){ambiance.execute(nsfwMode, message, args, logger, "safe")}
+	
+	if (message.mentions.has(client.user)){ambiance.execute(nsfwMode, message, args, logger, "safe")}
 
 	if (!message.content.startsWith(prefix) || message.author.bot) return;
 
