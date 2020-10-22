b5d5211820e93233d62af9b496c492b0b9c08bcd
BuildTools <unconfigured@null.spigotmc.org>
2020-10-22 17:03:32 -0500

    added say command

---
+++ commands/say.js
@@ -0,0 +1,12 @@
+const Discord = require('discord.js');
+const { execute } = require('./tag');
+
+module.exports = {
+    name: 'say',
+    aliases: ['simon', 'repeat'],
+	async execute(message, args, client, currency, logger) {
+        message.delete()
+        argsProssesed = args.join(" ")
+        message.channel.send(argsProssesed)
+	},
+};
