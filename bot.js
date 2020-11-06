e11018293348776fb3f233553b1f4c0fcf15f284
BuildTools <unconfigured@null.spigotmc.org>
2020-11-06 09:39:53 -0600

    added better furry detection

--- bot.js
+++ bot.js
@@ -214,13 +214,14 @@ client.on('message', async message => {
 	if (message.channel.type != 'dm' && passiveMoney == "true") {currency.add(message.author.id, 1);}
 	
 	
-	let preExtraArgs = message.content.toLowerCase().split(/ +/);
+	let preExtraArgs = message.content.replace(/[^a-zA-Z0-9 ]/g, "").toLowerCase().split(/ +/);
+	console.log(preExtraArgs)
 	let extraArgs = preExtraArgs;
 
 	if (extraArgs.includes("uwu")){
 		message.channel.send("> Don’t you dare say uwu again or I will break your shins. \n ~Drawgon")
 	}
-	else if (extraArgs.includes("owo")){
+	else if (extraArgs.includes("owo") || extraArgs.includes("x3") || extraArgs.includes("nuzzles")){
 		message.channel.send("> Keep your furry bullshit away from me. \n ~Ace")
 	}
 	
